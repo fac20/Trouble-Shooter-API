4535e07d163a6dd17fbfa3d62519f40015504551
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-09-19 19:04:04 +0100

    add schema

---
+++ src/database/init.sql
@@ -0,0 +1,27 @@
+BEGIN;
+
+DROP TABLE IF EXISTS categories, prompts, users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    cat_name VARCHAR(255) NOT NULL,
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE prompts (
+    id SERIAL PRIMARY KEY,
+    cat_id INTEGER REFERENCES categories(id),
+    prompt TEXT NOT NULL,
+    route_one_txt VARCHAR(255),
+    route_one INTEGER,
+    route_two_txt VARCHAR(255),
+    route_two INTEGER
+);
+
+COMMIT;
